de1fdb9478283c869caefc0cf9630a9af8101c22
shawn <liushawn@outlook.com>
2020-06-25 10:44:23 +0800

    two sum

---
+++ js/array/01 two sum/two sum.js
@@ -0,0 +1,47 @@
+// 方法一：两个for循环，时间复杂度平方
+var twoSum = function(nums, target) {
+    let length = nums.length;
+    for(let i=0; i<length; i++) {
+        let complement = target - nums[i];
+        for(let j = i + 1; j < length; j++) {
+            if(nums[j] == complement) {
+                return [i, j];
+            }
+        }
+    }
+    return [0, 0];
+};
+
+// 方法二：遍历两次2n
+var twoSum2 = function(nums, target) {
+    let length = nums.length;
+    let hashTable = {};
+    for(let i = 0; i < length; i++) {
+        hashTable[nums[i]] = i;
+    }
+    for(let i=0; i < length; i++) {
+        let complement = target - nums[i];
+        let found = hashTable[complement];
+        if(found != undefined && found != i) return [i, found];
+    }
+    return [0, 0];
+};
+
+// 方法三：遍历1次
+var twoSum3 = function(nums, target) {
+    let obj = {};
+    for(let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        let found = obj[complement];
+        if( found != undefined && found != i) return [found, i];
+        else {
+            obj[nums[i]] = i;
+        }
+    }
+    return [0, 0];
+}
+
+let arr = [2,7,11,15];
+let target = 9;
+let result = twoSum3(arr, target);
+console.log(result);
---
+++ js/test.js
@@ -0,0 +1,3 @@
+// vsCode安装codeRunner插件可以直接运行代码
+let people = 'hello world';
+console.log(people);
