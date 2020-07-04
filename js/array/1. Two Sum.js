72e475a389ffa5433c33459fcd7cb0c95e2f17a3
shawn <liushawn@outlook.com>
2020-07-05 00:27:33 +0800

    classify the file

--- js/array/1. Two Sum.js
+++ js/array/1. Two Sum.js
@@ -41,6 +41,20 @@ var twoSum3 = function(nums, target) {
     return [0, 0];
 }
 
+// 使用map,遍历一次
+var twoSum = function(nums, target) {
+    let map = new Map();
+    for(let i=0; i<nums.length; i++) {
+        let complement = target - nums[i];
+        if(map.has(complement)) {
+            return [map.get(complement), i];
+        } else {
+            map.set(nums[i], i);
+        }
+    }
+    return [0,0];
+};
+
 let arr = [2,7,11,15];
 let target = 9;
 let result = twoSum3(arr, target);
